075f974e2f2104e148720e8f20aebb7edff657d5
konnik <niklas.konstenius@gmail.com>
2020-03-30 21:15:07 +0200

    cacha anrop till smhi i en timme

--- smhi.py
+++ smhi.py
@@ -1,9 +1,19 @@
 
 import requests
 import time
+from cachetools import cached, TTLCache
+import logging
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
 
+CACHE_MAX_ITEMS = 100
+CACHE_TTL = 60*60 # enhet i sekunder
+
+@cached(cache = TTLCache(CACHE_MAX_ITEMS,CACHE_TTL))
 def fetch_data(subid):
+    log.info("Laddar data för subid %s", subid)
+
     url = "http://vattenwebb.smhi.se/hydronu/data/point?subid={}".format(subid)
 
     res = requests.get(url)
